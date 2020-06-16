a13183c7348ff0ab649f5c970a65ea7cee99271b
tim wu <timwuuk@gmail.com>
2020-06-16 23:26:31 +0100

    setup express, routes and passport

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const authRoutes = require("./routes/auth-routes");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use("/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(3000, () => {
+  console.log("app now listening for request on port 3000");
+});
---
+++ routes/auth-routes.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+
+//auth login
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+router.get("/logout", (req, res) => {
+  res.send("logging out");
+});
+
+//auth with google
+router.get("/google", (req, res) => {
+  res.send("logging in with google");
+});
+
+module.exports = router;
